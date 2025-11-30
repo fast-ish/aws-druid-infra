dde5cacd7d14f1034d0496cb5de92431f89578c1
stxkxs <brandon.stokes@hey.com>
2025-11-30 00:14:41 -0800

    update context variables from host/hosted to platform/deployment

--- src/main/java/fasti/sh/eks/Launch.java
+++ src/main/java/fasti/sh/eks/Launch.java
@@ -8,7 +8,6 @@ import fasti.sh.eks.stack.DruidReleaseConf;
 import fasti.sh.eks.stack.DruidStack;
 import fasti.sh.execute.serialization.Mapper;
 import fasti.sh.execute.serialization.Template;
-import fasti.sh.model.aws.cdk.Synthesizer;
 import fasti.sh.model.main.Common;
 import fasti.sh.model.main.Release;
 import java.util.ArrayList;
@@ -62,68 +61,18 @@ public class Launch {
         "conf.mustache",
         Map
           .ofEntries(
-            Map.entry("hosted:eks:druid:release", app.getNode().getContext("hosted:eks:druid:release").toString()),
-            Map.entry("hosted:tags", tags(app))));
+            Map.entry("deployment:eks:druid:release", app.getNode().getContext("deployment:eks:druid:release").toString()),
+            Map.entry("deployment:tags", tags(app))));
     var type = new TypeReference<Release<DruidReleaseConf>>() {};
     return Mapper.get().readValue(parsed, type);
   }
 
-  @SneakyThrows
   private static StackSynthesizer synthesizer(App app) {
-    var customSynthesizer = Boolean.parseBoolean(app.getNode().getContext("synthesizer:custom").toString());
-
-    if (customSynthesizer) {
-      var key = app.getNode().getContext("hosted:api:key");
-      var synthesizerName = app.getNode().getContext("hosted:synthesizer:name");
-      var synthesizerAlias = app.getNode().getContext("hosted:synthesizer:alias");
-
-      var parsed = Template
-        .parse(
-          app,
-          "synthesizer.mustache",
-          Map
-            .ofEntries(
-              Map.entry("synthesizer:role:exec", app.getNode().getContext("hosted:cdk:role:exec").toString()),
-              Map.entry("synthesizer:role:deploy", app.getNode().getContext("hosted:cdk:role:deploy").toString()),
-              Map.entry("synthesizer:role:lookup", app.getNode().getContext("hosted:cdk:role:lookup").toString()),
-              Map.entry("synthesizer:role:assets", app.getNode().getContext("hosted:cdk:role:assets").toString()),
-              Map.entry("synthesizer:role:images", app.getNode().getContext("hosted:cdk:role:images").toString()),
-              Map.entry("synthesizer:ssm", app.getNode().getContext("hosted:cdk:keys:ssm").toString()),
-              Map.entry("synthesizer:storage:assets", app.getNode().getContext("hosted:cdk:storage:assets").toString()),
-              Map.entry("synthesizer:storage:images", app.getNode().getContext("hosted:cdk:storage:images").toString()),
-              Map.entry("synthesizer:qualifier", String.format("%s-%s", synthesizerName, synthesizerAlias)),
-              Map.entry("synthesizer:assets:prefix", String.format("%s-%s", synthesizerName, synthesizerAlias)),
-              Map.entry("synthesizer:images:tag", String.format("%s-%s", synthesizerName, synthesizerAlias)),
-              Map.entry("synthesizer:externalid", key)));
-
-      var synthesizer = Mapper.get().readValue(parsed, Synthesizer.class);
-      return DefaultStackSynthesizer.Builder
-        .create()
-        .qualifier(synthesizer.qualifier())
-        .cloudFormationExecutionRole(synthesizer.cloudFormationExecutionRole())
-        .deployRoleArn(synthesizer.deployRoleArn())
-        .lookupRoleArn(synthesizer.lookupRoleArn())
-        .fileAssetPublishingRoleArn(synthesizer.fileAssetPublishingRoleArn())
-        .imageAssetPublishingRoleArn(synthesizer.imageAssetPublishingRoleArn())
-        .bootstrapStackVersionSsmParameter(synthesizer.bootstrapStackVersionSsmParameter())
-        .fileAssetsBucketName(arnToBucketName(synthesizer.fileAssetsBucketName()))
-        .imageAssetsRepositoryName(synthesizer.imageAssetsRepositoryName())
-        .generateBootstrapVersionRule(synthesizer.generateBootstrapVersionRule())
-        .useLookupRoleForStackOperations(synthesizer.useLookupRoleForStackOperations())
-        .bucketPrefix(synthesizer.bucketPrefix())
-        .dockerTagPrefix(synthesizer.dockerTagPrefix())
-        .lookupRoleExternalId(synthesizer.lookupRoleExternalId())
-        .fileAssetPublishingExternalId(synthesizer.fileAssetPublishingExternalId())
-        .imageAssetPublishingExternalId(synthesizer.imageAssetPublishingExternalId())
-        .deployRoleExternalId(synthesizer.deployRoleExternalId())
-        .build();
-    } else {
-      return DefaultStackSynthesizer.Builder.create().build();
-    }
+    return DefaultStackSynthesizer.Builder.create().build();
   }
 
   private static ArrayList<Map<String, String>> tags(App app) {
-    var tags = app.getNode().getContext("hosted:tags");
+    var tags = app.getNode().getContext("deployment:tags");
     var results = new ArrayList<Map<String, String>>();
     if (tags instanceof List<?> tagList) {
       for (var tag : tagList) {
